3de18ff47bb43f07badca1a417dc3ac0a756ce91
Giovanni Pinto <giovanni.pinto@appliedblockchain.com>
2024-12-03 11:25:01 +0100

    WIP problem dampening

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,18 +1,31 @@
 use std::{fs::File, io::{self, BufRead}};
 
-fn calculate_safety(levels: &Vec<u8>) -> bool {
+fn calculate_safety(levels: &Vec<u8>, dampened: bool) -> bool {
     let order = levels[1] > levels[0];
     for i in 1..levels.len() {
         if (levels[i] > levels[i - 1]) != order {
-            return false
+            if dampened {
+                return false
+            }
+            let mut dampened = levels.clone();
+            dampened.remove(i);
+            return calculate_safety(&dampened, true);
         }
         if (levels[i].abs_diff(levels[i - 1])) == 0 {
-            return false
-        }
+            if dampened {
+                return false
+            }
+            let mut dampened = levels.clone();
+            dampened.remove(i);
+            return calculate_safety(&dampened, true);        }
 
         if (levels[i].abs_diff(levels[i - 1])) > 3 {
-            return false
-        }
+            if dampened {
+                return false
+            }
+            let mut dampened = levels.clone();
+            dampened.remove(i);
+            return calculate_safety(&dampened, true);        }
     }
     true
 }
@@ -27,7 +40,7 @@ fn main() -> io::Result<()> {
         if let Ok(line) = line {
             let parts = line.split_whitespace();
             let levels = parts.map(|x| x.parse::<u8>().unwrap()).collect();
-            let safe = calculate_safety(&levels);
+            let safe = calculate_safety(&levels, false);
             println!("Levels: {:?}, Safe: {}", levels, safe);
             if safe {
                 score += 1;
