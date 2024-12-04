63368bc494f5cdc7135f26012479bba57a640356
Giovanni Pinto <jop.mezzana@gmail.com>
2024-12-04 10:31:42 +0100

    WIP

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,65 +1,21 @@
-use std::{fs::File, io::{self, BufRead}};
-
-fn calculate_safety(levels: &Vec<u8>, dampened: bool) -> bool {
-    let mut last_order = levels[0] > levels[1];
-    for i in 0..levels.len() - 1 {
-        let mut dampened_left = levels.clone();
-        dampened_left.remove(if i == 0 { i } else { i -1 });
-        let mut dampened_current = levels.clone();
-        dampened_current.remove(i);
-        let mut dampened_right = levels.clone();
-        dampened_right.remove(if i + 1 < levels.len() { i + 1 } else { i });
-
-        
-
-        if (levels[i] > levels[i + 1]) != last_order {
-            if dampened {
-                return false
-            }
-            return calculate_safety(&dampened_left, true) || calculate_safety(&dampened_current, true) || calculate_safety(&dampened_right, true);
-        }
-        last_order = levels[i] > levels[i + 1];
-
-        if (levels[i].abs_diff(levels[i + 1])) == 0 {
-            if dampened {
-                return false
-            }
-            return calculate_safety(&dampened_left, true) || calculate_safety(&dampened_current, true) || calculate_safety(&dampened_right, true);
-        }
-
-        if (levels[i].abs_diff(levels[i + 1])) > 3 {
-            if dampened {
-                return false
-            }
-            return calculate_safety(&dampened_left, true) || calculate_safety(&dampened_current, true) || calculate_safety(&dampened_right, true);
-        }
-    }
-    true
-}
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+use regex::Regex;
 
 fn main() -> io::Result<()> {
-    let path = "input.txt";
-    let input = File::open(&path)?;
-    let buffered = io::BufReader::new(input);
+    let mut f = File::open("example.txt")?;
+    let mut buffer = String::new();
 
-    let test = vec![10, 12, 11, 10];
-    let safe = calculate_safety(&test, false);
-    println!("Levels: {:?}, Safe: {}", test, safe);
+    f.read_to_string(&mut buffer)?;
 
-    let mut score = 0;
-    for line in buffered.lines() {
-        if let Ok(line) = line {
-            let parts = line.split_whitespace();
-            let levels = parts.map(|x| x.parse::<u8>().unwrap()).collect();
-            let safe = calculate_safety(&levels, false);
-            println!("Levels: {:?}, Safe: {}", levels, safe);
-            if safe {
-                score += 1;
-            }
-        }
-    }
+    // regex matching
+    let re = Regex::new(r"(mul\((\d{0,3}),(\d{0,3})\))").unwrap();
 
-    println!("Score: {}", score);
+    let results = re.find_iter( &buffer).map(|m| {
+        println!("Found match: {}", &buffer[m.start()..m.end()]);
+        m.as_str()
+    });
 
     Ok(())
 }
